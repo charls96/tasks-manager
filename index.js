45ebe9b8f2a9d26566d39fcb1cc4d8a274e1e91f
charls96 <charly.lopez.perez@gmail.com>
2022-08-13 19:42:03 +0200

    user first routes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectionDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js"
 
 const app = express();
 dotenv.config();
 connectionDB();
+
+// Routing
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 4000;
 
