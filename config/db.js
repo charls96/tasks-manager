a8a48569217c92adbedb5ba5f69e8ec87ac6e724
charls96 <charly.lopez.perez@gmail.com>
2022-08-13 20:47:08 +0200

    avoiding duplicated users registrations

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const connectDB = async () => {
     const connection = await mongoose.connect(
       process.env.MONGO_URI, {
           useNewUrlParser: true,
-          useUnifiedTopology: true
+          useUnifiedTopology: true,
+          dbName: "tasksManager"
       }
     );
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,15 @@
 import User from "../models/User.js";
 
 const register = async (req, res) => {
+  //Reject duplicated registrations
+  const { email } = req.body;
+  const existsUser = await User.findOne({ email })
+
+  if(existsUser){
+    const error = new Error("Email alredy exists")
+    return res.status(400).json({msg: error.message})
+  }
+
   try {
     const user = new User(req.body);
     const insertUser = await user.save();
